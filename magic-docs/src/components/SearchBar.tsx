033d7ac503e2c6b3d6e28e79ed293a2bf05d1b02
kairveeehh <jampu741@gmail.com>
2024-06-03 15:22:37 +0530

    query

--- magic-docs/src/components/SearchBar.tsx
+++ magic-docs/src/components/SearchBar.tsx
@@ -1,14 +1,41 @@
+"use client"
+
 import { Loader2, Search } from "lucide-react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { useRef, useState, useTransition } from "react";
+import { useRouter } from "next/navigation";
 
 
 const SearchBar = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [isSearching , startTransition] = useTransition()
+  const router  = useRouter()
+  const [query , setQuery] = useState<string>('')
+  const search = () =>{
+    startTransition(() => {
+   router.push(`/search?query=${query}`)
+    }
+  )
+  }
+
+
   return (
     <div className="relative w-full h-14 flex flex-col bg-white">
       <div className="relative h-14 z-10 rounded-md">
-       <Input className="absolute h-full inset-0"/>
-       <Button/>
+       <Input 
+       value = {query}
+       onChange={(e) => setQuery(e.target.value)}
+       onKeyDown={(e) => {
+
+        if(e.key ==="Enter"){
+          search()
+        }
+        if(e.key ==="Escape"){
+          inputRef?.current?.blur();
+        }
+       }} ref ={inputRef} className="absolute h-full inset-0"/>
+       <Button onClick={search}/>
       </div>
     </div>
   );
