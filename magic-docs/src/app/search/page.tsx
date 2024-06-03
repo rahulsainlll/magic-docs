fbad3ad83578caca960d7120afaf7c1b810f8c44
rahulsainlll <rahulsain.social777@gmail.com>
2024-06-03 21:13:42 +0530

    Search-init

--- magic-docs/src/app/search/page.tsx
+++ magic-docs/src/app/search/page.tsx
@@ -1,5 +1,6 @@
-// "use client";
-
+import { db } from "@/db";
+import { docsTable } from "@/db/schema";
+import { sql } from "drizzle-orm";
 import { redirect } from "next/navigation";
 
 interface PageProps {
@@ -8,16 +9,29 @@ interface PageProps {
   };
 }
 
-const Page = ({ searchParams }: PageProps) => {
+const Page = async ({ searchParams }: PageProps) => {
   const query = searchParams.query;
 
   if (Array.isArray(query) || !query) {
     return redirect("/");
   }
 
+  let docs = await db
+    .select()
+    .from(docsTable)
+    .where(
+      sql`to_tsvector('simple', lower(${docsTable.title} || ' ' || ${
+        docsTable.description
+      })) @@ to_tsquery('simple', lower(${query
+        .trim()
+        .split(" ")
+        .join(" & ")}))`
+    )
+    .limit(3);
+
   return (
     <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex gap-16 lg:px-8 lg:py-24">
-      <p>hello</p>
+      <pre>{JSON.stringify(docs)}</pre>
     </div>
   );
 };
--- magic-docs/src/db/schema.ts
+++ magic-docs/src/db/schema.ts
@@ -1,11 +1,13 @@
-import { pgTable , text } from "drizzle-orm/pg-core";
+import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
 
-export const docsTable = pgTable("docs" , {
-  id: text("id").primaryKey().default('uuid_generate_v4()'),
-  title: text('title').notNull(),
-  description: text('description'),
-  author: text('author').notNull(),
+export const docsTable = pgTable("docs", {
+  id: text("id").primaryKey().default("uuid_generate_v4()"),
+  title: text("title").notNull(),
+  description: text("description"),
+  author: text("author").notNull(),
+  content: text("content").notNull(),
+  createdAt: timestamp("createdAt").defaultNow(),
+  updatedAt: timestamp("updatedAt").defaultNow(),
+});
 
-})
-
-export type Docs = typeof docsTable.$inferSelect
+export type Docs = typeof docsTable.$inferSelect;
