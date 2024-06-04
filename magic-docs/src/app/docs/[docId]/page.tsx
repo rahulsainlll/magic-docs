cac6d5d9e23695046fb1610194cd8a2b2dcc956e
rahulsainlll <rahulsain.social777@gmail.com>
2024-06-04 21:16:01 +0530

    ui

--- magic-docs/src/app/docs/[docId]/page.tsx
+++ magic-docs/src/app/docs/[docId]/page.tsx
@@ -28,47 +28,47 @@ const Page = async ({ params }: PageProps) => {
   if (!docs) return notFound()
 
   return (
-    <div className='py-8 pb-8 px-12 divide-y divide-zinc-100 bg-white shadow-md rounded-b-md'>
-      <div>
-      <BackButton />
+    <div className="py-8 pb-8 px-12 divide-y">
+      <div className="max-w-lg lg:max-w-3xl mx-auto">
+        <BackButton />
 
-        <div className='mt-4'>
-          <h1 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
+        <div className="mt-4">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             {}
           </h1>
         </div>
-
-        <div className='aspect-square my-6 border border-border w-52 h-52'>
-          <div className='relative bg-zinc-100 w-full h-full overflow-hidden rounded-xl'>
-            
-          </div>
+        
+        <div className="aspect-auto my-6 border border-border w-full h-52">
+          <div className="relative bg-zinc-100 w-full h-full overflow-hidden rounded-xl"></div>
         </div>
 
-        <div className='mt-4'>
-          <div className='flex items-center'>
-            <p className='font-medium text-gray-900'>
-              {docs.author}
-            </p>
+        <div className="mt-4">
+          <div className="flex items-center">
+            <p className="font-medium text-3xl text-gray-900">{docs.title}</p>
           </div>
 
-          <div className='mt-4 space-y-6'>
-            <p className='text-base max-w-prose text-muted-foreground'>
+          <div className="mt-4 space-y-6">
+            <p className="text-2xl max-w-prose text-muted-foreground">
               {docs.description}
             </p>
           </div>
 
-          <div className='mt-6 flex items-center'>
-            <Check className='h-5 w-5 flex-shrink-0 text-green-500' />
-            <p className='ml-2 text-sm text-muted-foreground'>
+          <div className="mt-4 space-y-6">
+            <p className="text-xl max-w-prose text-muted-foreground break-words">
+              {docs.content}
+            </p>
+          </div>
+
+          <div className="mt-6 flex items-center">
+            <Check className="h-5 w-5 flex-shrink-0 text-green-500" />
+            <p className="ml-2 text-sm text-muted-foreground">
               Verified by MagicDocs
             </p>
           </div>
         </div>
       </div>
-
-    
     </div>
-  )
+  );
 }
 
 export default Page
